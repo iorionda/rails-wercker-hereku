ce37c850f1953045e2a234c66d761acdd5a9537b
Iori ONDA <iori.onda@gmail.com>
2014-01-09 00:55:50 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsWerckerHereku::Application.config.secret_key_base = '2e1f9d6233220ed0b7e26421a46f7204e65c9bd67fa90f7e4577ed63d77b7c297545e02b41733ac0cdeed1959a84f89e880308e76d922d57851a98b7003b3973'
